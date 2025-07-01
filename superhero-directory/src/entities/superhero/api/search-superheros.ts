6bfc65b11276da54eef1568003c79afb40f65cc7
nickiskit <nickiskit@gmail.com>
2025-07-01 21:03:36 +0500

    Add search superheros function

--- superhero-directory/src/entities/superhero/api/search-superheros.ts
+++ superhero-directory/src/entities/superhero/api/search-superheros.ts
@@ -1,14 +1,13 @@
-/*
 import { config } from '~shared/config';
 import { ResponseError, ResponseSuccess } from '~shared/response';
 
-import { useQuery } from '@tanstack/react-query';
+import { skipToken, useQuery } from '@tanstack/react-query';
 
 import { superheroKeys } from './keys';
 
 import { Superhero } from '../superhero';
 
-type ResponsePayload = {
+export type ResponsePayload = {
   'results-for': string;
   results: Superhero[];
 };
@@ -22,6 +21,32 @@ export function useSearchSuperheros(params: Params) {
 
   // Method documentation: https://superheroapi.com/#name
   // Example call: GET https://superheroapi.com/api/${access-token}/search/${superhero-name}
-  return useQuery({});
+  return useQuery({
+    queryKey: superheroKeys.search(query ?? ''),
+    queryFn: query
+      ? async () => {
+          const response: ResponseSuccess<ResponsePayload> | ResponseError =
+            await fetch(
+              `${config.apiHost}/api/${config.apiToken}/search/${encodeURIComponent(query)}`,
+              {
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+              }
+            ).then(async (res) => {
+              if (!res.ok) {
+                const error: ResponseError = await res.json();
+
+                throw new Error(
+                  `Error ${res.status}: ${res.statusText} - ${error.error}`
+                );
+              }
+
+              return res.json();
+            });
+
+          return response;
+        }
+      : skipToken,
+  });
 }
- */
